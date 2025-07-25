d3a91926bd969c7b3aee89c3c953e5e6aa96f0f1
Ezequiel <ezequiel.cardoso@hexing.com.br>
2025-07-25 11:36:23 -0300

    attribute Scaler and stat calculator

---
+++ Calculators/StatCalculator.cs
@@ -0,0 +1,34 @@
+﻿using RPG.Attributes;
+using RPG.Enums;
+
+namespace RPG.Calculators;
+
+public class StatCalculator
+{
+    public static float ComputeBonus(CharacterAttribute attr, int points)
+    {
+        var scaler = GetScalerFor(attr);
+        return scaler.CalculateBonus(points);
+    }
+
+    private static AttributeScaler GetScalerFor(CharacterAttribute attr)
+    {
+        return attr switch
+        {
+            CharacterAttribute.Vitality => new AttributeScaler
+                { GrowthLinear = 10f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Vigor => new AttributeScaler { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Strength => new AttributeScaler
+                { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Dexterity => new AttributeScaler
+                { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Intelligence => new AttributeScaler
+                { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Faith => new AttributeScaler { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Endurance => new AttributeScaler
+                { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            CharacterAttribute.Arcane => new AttributeScaler { GrowthLinear = 5f, GrowthSoft = 2f, GrowthHard = 0.5f },
+            _ => new AttributeScaler()
+        };
+    }
+}
